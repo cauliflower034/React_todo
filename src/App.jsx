c752ef50a6f740044f801ce73dbdf2feab33a4ad
Shingo <93265847+cauliflower034@users.noreply.github.com>
2022-01-08 14:34:18 +0900

    Todoの完成 リファクタリング前

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,80 @@
+import React, { useState } from "react";
 import "./styles.css";
 
-export default function App() {
+export const App = () => {
+  const [TodoText, setTodoText] = useState("");
+  const [incmp, setincmp] = useState([]);
+  const [cmp, setcmp] = useState([]);
+
+  const onChangeTodoText = (event) => setTodoText(event.target.value);
+
+  const onClickAdd = () => {
+    if (TodoText === "") return;
+    const newTodos = [...incmp, TodoText];
+    setincmp(newTodos);
+    setTodoText("");
+  };
+
+  const onClickdel = (index) => {
+    const newTodos = [...incmp];
+    newTodos.splice(index, 1);
+    setincmp(newTodos);
+  };
+
+  const onClickCmp = (index) => {
+    const newincmpTodos = [...incmp];
+    newincmpTodos.splice(index, 1);
+    setincmp(newincmpTodos);
+
+    const newcmpTodos = [...cmp, incmp[index]];
+    setcmp(newcmpTodos);
+  };
+
+  const onClickBack = (index) => {
+    const newcmpTodos = [...cmp];
+    newcmpTodos.splice(index, 1);
+
+    const newincmpTodos = [...incmp, cmp[index]];
+    setcmp(newcmpTodos);
+    setincmp(newincmpTodos);
+  };
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
+    <>
+      <div className="input-area">
+        <input
+          placeholder="TODO入力"
+          value={TodoText}
+          onChange={onChangeTodoText}
+        />
+        <button onClick={onClickAdd}>追加</button>
+      </div>
+      <div className="incomplete-area">
+        <p className="title">未完了のTODO</p>
+        <ul>
+          {incmp.map((todo, index) => {
+            return (
+              <div key={todo} className="list-row">
+                <li>{todo}</li>
+                <button onClick={() => onClickCmp(index)}>完了</button>
+                <button onClick={() => onClickdel(index)}>削除</button>
+              </div>
+            );
+          })}
+        </ul>
+      </div>
+      <div className="complete-area">
+        <p className="title">完了したTODO</p>
+        <ul>
+          {cmp.map((todo, index) => {
+            return (
+              <div key={todo} className="list-row">
+                <li>{todo}</li>
+                <button onClick={() => onClickBack(index)}>戻す</button>
+              </div>
+            );
+          })}
+        </ul>
+      </div>
+    </>
   );
-}
+};
